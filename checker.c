40f91e771acb0be763d7c02b8a801cbc0558c958
Shwetha1235 <shwethaam333@gmail.com>
2024-08-28 10:55:39 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -30,16 +30,16 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
  
 int main() {
 // Temperature < 0, rest optimal
-  assert(!battery_is_ok(-0.1, 70, 0.1));
+  assert(!batteryIsOk(-0.1, 70, 0.1));
   // Temperature > 45, rest optimal
-  assert(!battery_is_ok(50.1, 70, 0.3));
+  assert(!batteryIsOk(50.1, 70, 0.3));
   // SOC < 20, rest optimal
-  assert(!battery_is_ok(30, 17, 0.3) );
+  assert(!batteryIsOk(30, 17, 0.3) );
   // SOC > 80, rest optimal
-  assert(!battery_is_ok(26, 90, 0.5));
+  assert(!batteryIsOk(26, 90, 0.5));
   // Charge Rate > 0.8, rest optimal
-  assert(!battery_is_ok(30, 60, 0.9));
+  assert(!batteryIsOk(30, 60, 0.9));
   // All optimal values
-  assert(battery_is_ok(30, 60, 0.7));
+  assert(batteryIsOk(30, 60, 0.7));
    return 0;
 }
