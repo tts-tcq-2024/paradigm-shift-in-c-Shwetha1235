a357d7764ec0000bc1d7ad817f3dfde17c47617c
Shwetha1235 <shwethaam333@gmail.com>
2024-10-17 11:36:07 +0530

    Update chargeRateCheck.h

--- chargeRateCheck.h
+++ chargeRateCheck.h
@@ -1,4 +1,4 @@
-int Charge_Ok(float charge_rate,int language) {
+int charge_Ok(float charge_rate,int language) {
    if (charge_rate > 0.8) {
        printMessage(chargeRateMessage[language]);
        return 0; 
