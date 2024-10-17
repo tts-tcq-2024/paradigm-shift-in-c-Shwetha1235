77139525982b0a47b341d50ce98fe158b2f2e268
Shwetha1235 <shwethaam333@gmail.com>
2024-10-17 11:09:54 +0530

    Update batteryStatusMessages.h

--- batteryStatusMessages.h
+++ batteryStatusMessages.h
@@ -1,3 +1,3 @@
-const char *temperatureMessage[] = {"","Temperature out of range!", "Temperatur außerhalb des zulässigen Bereichs!"};
-const char *socMessage[] = {"","State of Charge out of range!", "Ladezustand außerhalb des Bereichs!"};
-const char *chargeRateMessage[] = {"", "Charge Rate out of range!", "Laderate außerhalb des Bereichs!"};
+const char *temperatureMessage[] = {"","Temperature out of range!"};
+const char *socMessage[] = {"","State of Charge out of range!"};
+const char *chargeRateMessage[] = {"", "Charge Rate out of range!"};
