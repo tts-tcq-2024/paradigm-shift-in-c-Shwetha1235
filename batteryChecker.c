ffbc1a523438cca75e7a16cdb693b63d5bd0d551
Shwetha1235 <shwethaam333@gmail.com>
2024-10-17 11:33:35 +0530

    Update batteryChecker.c

--- batteryChecker.c
+++ batteryChecker.c
@@ -12,11 +12,11 @@ int batteryIsOk(float temperature, float soc, float charge_rate,int language) {
 
 int main() {
 
-  assert(!batteryIsOk(-0.2, 70, 0.1));
-  assert(!batteryIsOk(50.1, 70, 0.3));
-  assert(!batteryIsOk(25, 18, 0.3) );
-  assert(!batteryIsOk(26, 90, 0.5));
-  assert(!batteryIsOk(30, 60, 0.9));
-  assert(batteryIsOk(25, 60, 0.7));
+  assert(!batteryIsOk(-0.2, 70, 0.1,1));
+  assert(!batteryIsOk(50.1, 70, 0.3,1));
+  assert(!batteryIsOk(25, 18, 0.3,2) );
+  assert(!batteryIsOk(26, 90, 0.5,1));
+  assert(!batteryIsOk(30, 60, 0.9,2));
+  assert(batteryIsOk(25, 60, 0.7,1));
 }
  
