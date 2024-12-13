a791d4d6be0226c6db55094bec43abd0ad37099b
Shwetha1235 <shwethaam333@gmail.com>
2024-12-13 16:12:50 +0530

    Update and rename batteryStatusMessages.h to batteryStatusMessages.c

--- batteryStatusMessages.h
+++
@@ -1,3 +0,0 @@
-const char *temperatureMessage[] = {"","Temperature out of range!"};
-const char *socMessage[] = {"","State of Charge out of range!"};
-const char *chargeRateMessage[] = {"", "Charge Rate out of range!"};
