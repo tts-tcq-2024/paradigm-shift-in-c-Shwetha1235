941762d1eb385bea3dbb106148172a65f613dc32
Shwetha1235 <shwethaam333@gmail.com>
2024-12-13 16:37:09 +0530

    Update batteryStatusMessages.c

--- batteryStatusMessages.c
+++ batteryStatusMessages.c
@@ -1,34 +1,13 @@
-#include <stdio.h>
-#include <assert.h>
- 
-int tempcheck(float temperature){
-    if(temperature < 0 || temperature > 45){
-        printf("Temperature out of range!\n");
-        return 0;
-    }
-    return 1;
+#include <stdio.h>                                                 
+ void checkEvenOdd(int number) {
+    if (number % 2 == 0) {
+       printf("%d is Even\n", number);
+    } else {
+        printf("%d is Odd\n", number);
+    }
 }
-int soccheck(float soc){
-    if(soc < 20 || soc > 80){
-        printf("State of Charge out of range!\n");
-        return 0;
-    }
-    return 1;
-}
-int chargecheck(float chargeRate){
-    if(chargeRate > 0.8){
-        printf("Charge Rate out of range!\n");
-        return 0;
-    }
-    return 1;
-}
- 
- 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-   return tempcheck(temperature) && soccheck(soc) && chargecheck(chargeRate);
-}
- 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+    checkEvenOdd(5);
+    checkEvenOdd(10);
+    return 0;
 }
